01cc684fd5fb3a84349cf8be9372eedbf191ebf7
bayulee <bayuleepaul@gmail.com>
2022-03-21 15:49:25 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import ProfileCard from "./component/ProfileCard";
+
+
+class App extends Component {
+ constructor(){
+   super();
+   this.handleClick =this.handleClick.bind(this);
+   this.state ={
+     writers:{
+       loading: false,
+       list:[]
+     }
+   };
+ }
+ handleClick(){
+   this.setState({
+     writers:{
+       loading:true
+     }
+   });
+
+ setTimeout(async()=>{
+   let resp=await fetch("/writers.json");
+   let result =await resp.json();
+  
+   this.setState({
+     writers:{
+       loading:false,
+       list:result
+     }
+   });
+ }, 3500);
+ 
+   }
+   render(){
+     const{
+       writers:{loading,list}
+     } = this.state;
+     if(loading){
+       return(     <div>
+        <h1>writers profile</h1>
+        <div className="container">
+        <div className="card action">
+        <p className="infoText">loading....</p>
+        </div>
+        </div>
+        </div>);
+ 
+     }
+     return(
+       <div>
+       <h1>writers profile</h1>
+       <div className="container">
+       {list.length=== 0 ?(
+         <div className="card action">
+         
+         <p className="infoText"> oops ... no writer profile found</p>
+         <button className="actionBtn" onClick={this.handleClick}> Get writers</button>
+         </div>
+       ) : (
+         list.map((writer) =>(
+           <profileCard key={writer.id} writer={writer}/>
+       ))
+       )}
+       </div>
+       </div>
+     );
+         }
+        }
+
+
+export default App;   
+
+
+
+  
+    
+    
+  
+
